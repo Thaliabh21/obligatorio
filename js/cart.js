be05446c93419f34f59586777476999905ad7a97
Alicia <aliciataliaromero@gmail.com>
2022-10-02 20:15:29 -0300

    Declarando usuario y seteando el correo

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,12 @@
+let user = sessionStorage.getItem("correo");
+
+    if (user !== null){
+        document.getElementById("correo").innerHTML=user;    
+    } else {
+        alert("Debe iniciar sesión para poder continuar")
+        location.href="login.html";
+    }
+
 document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("cerrar").addEventListener("click", function() {
     sessionStorage.removeItem("correo"); // Elimino el usuario guardado anteriormente en el almacenamiento de sesión.
