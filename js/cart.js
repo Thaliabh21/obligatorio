40340883567a8c8d0dd8e3c0ced33ec9fe73eaf8
Alicia <aliciataliaromero@gmail.com>
2022-10-16 22:19:21 -0300

    Realizando entregable 5

--- js/cart.js
+++ js/cart.js
@@ -13,3 +13,35 @@ document.addEventListener("DOMContentLoaded", function(e){
     window.location = "login.html"
     });
 });
+
+let articulos=[];
+
+function cambiarCantidad(){
+    let cant= document.getElementById("cantidad");
+        articulo.count = cant.value; // Le asigno a la propiedad articulo.count, el valor introducido en el input.
+        mostrarCarrito(articulos);
+}
+
+function mostrarCarrito(articulos){
+    let cart="";
+    for (articulo of articulos){
+        let resultado = articulo.count * articulo.unitCost;
+            cart+=`<tr>
+            <td><img src="` + articulo.image + `"alt="product image" class="img-thumbnail" width="100px"></img></td>
+            <td style="padding: 25px;">` + articulo.name + `</td>
+            <td style="padding: 25px;">` + articulo.currency + " " + articulo.unitCost + `</td>
+            <td style="padding: 25px;">` + `<input onchange="cambiarCantidad()" id="cantidad" type="number" min="1" value=` + articulo.count + ` style="width:70px;" ></input>` + `</td>
+            <td style="padding: 25px;"><strong>` + articulo.currency + " " + resultado + `</strong></td>
+            </tr>`
+    }
+    document.getElementById("cart").innerHTML= cart;
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL + "25801.json").then(function(resultObj){
+        if(resultObj.status === "ok"){
+            articulos=resultObj.data.articles;
+            mostrarCarrito(articulos);
+        }
+    })
+});
